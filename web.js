e1fd16a1f539ebd4232a608fd2b69fc08c5cd361
Bill Paseman <git@paseman.com>
2013-07-12 00:19:30 +0000

    let's try afain

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  var buf = fs.readFileSync('index.html');
+  var s = buf.toString(); //[encoding], [start], [end]);
+
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
